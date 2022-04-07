1e49d1276e880c05016ae6f5d49efa7d16fc2d22
frankxia <fan.xia@qq.com>
2022-04-07 11:06:30 +0800

    Update include.php

--- includes/prism-php/examples/include.php
+++ includes/prism-php/examples/include.php
@@ -3,8 +3,8 @@
 require_once('../lib/client.php');
 
 $url = 'http://127.0.0.1:8080/api';
-$key = 'xkg3ydnm';
-$secret = '56dygmyhrfuhuwrdst3c';
+$key = '';
+$secret = '';
 
 $c = new prism_client($url, $key, $secret);
 
